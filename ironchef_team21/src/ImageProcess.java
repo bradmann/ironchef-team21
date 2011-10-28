75856e07bb89645d0e56820d6e79f8219a06bfb7
bradmann <bradmann@gmail.com>
2011-10-27 22:37:29 -0700

    Calculate image phashes.

---
+++ ironchef_team21/src/ImageProcess.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
+
+
+public class ImageProcess {
+	public static void main(String[] args) {
+		try {
+			ImagePHash phash = new ImagePHash();
+			File f = new File("/Users/bradmann/Downloads/Basketball.png");
+			
+			FileInputStream iis = new FileInputStream(f);
+		
+			String hash = phash.getHash(iis);
+			System.out.println(hash);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ironchef_team21/src/ImageProcessor.java
@@ -0,0 +1,61 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.log4j.Logger;
+
+import com.marklogic.mapreduce.ContentOutputFormat;
+import com.marklogic.mapreduce.DocumentInputFormat;
+import com.marklogic.mapreduce.DocumentURI;
+import com.marklogic.mapreduce.KeyValueInputFormat;
+import com.marklogic.mapreduce.MarkLogicInputFormat;
+import com.marklogic.mapreduce.MarkLogicNode;
+import com.marklogic.mapreduce.NodePath;
+import com.marklogic.mapreduce.ValueInputFormat;
+
+public class ImageProcessor {
+
+	public static class PHashMapper extends Mapper<Text, Text, DocumentURI, Text>{
+	    
+		public void map(Text key, Text value, Context context) {
+			try {
+				ImagePHash phash = new ImagePHash();
+				InputStream is = new ByteArrayInputStream(value.toString().getBytes());		
+				Logger.getRootLogger().info("***************************");
+				Text hash = new Text(phash.getHash(is));
+				String newkey = key.toString() + "_phash";
+	           	context.write(new DocumentURI(newkey), hash);
+            } catch (Exception e) {
+            	e.printStackTrace();
+            }
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		//This is the line that makes the hadoop run locally
+		//conf.set("mapred.job.tracker", "local");
+		
+		Configuration conf = new Configuration();
+
+        Job job = new Job(conf, "Process Image Hash");
+        job.setJarByClass(ImageProcessor.class);
+        job.setInputFormatClass(KeyValueInputFormat.class);
+        job.setMapperClass(PHashMapper.class);
+        job.setMapOutputKeyClass(DocumentURI.class);
+        job.setMapOutputValueClass(MarkLogicNode.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        conf.set("mapred.reduce.tasks", "0");
+        
+        job.setOutputFormatClass(ContentOutputFormat.class);
+        
+        conf = job.getConfiguration();
+        conf.addResource("ImageProcessor.xml");
+    
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
